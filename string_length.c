b5ffeb9152f1ce4de758d0e14d5798456a69b245
Sumangal Karan <123644743+Sumangal44@users.noreply.github.com>
2025-01-20 20:16:40 +0530

    refactor string length calculation and improve input handling

--- string_length.c
+++ string_length.c
@@ -1,17 +1,16 @@
-#include<stdio.h>
-// #include<string.h>
-#include<stdlib.h>
-int main() 
+#include <stdio.h>
+#include <stdlib.h>
+int main()
 {
     char s[20];
-    int i,length=0;
+    int i, length = 0;
     system("cls");
     printf("Enter a string:");
-gets(s);
-for (i = 0; s[i]!='\0'; i++)
-{
-    length++;
-}
-    printf("Length of string is %d",length);
+    fgets(s, sizeof(s), stdin);
+    for (i = 1; s[i] != '\0'; i++)
+    {
+        length++;
+    }
+    printf("Length of string is %d", length);
     return 0;
 }
